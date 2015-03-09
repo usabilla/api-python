795b349231085c1c1eb14d095ac858bf6ceb2910
George Viorel <george@usabilla.com>
2015-03-09 13:06:38 +0100

    Updated test for query parameters

--- tests.py
+++ tests.py
@@ -49,7 +49,7 @@ class TestClient(TestCase):
         self.assertEqual(self.client.method, 'GET')
         self.assertEqual(self.client.host, 'data.usabilla.com')
         self.assertEqual(self.client.host_protocol, 'https://')
-        self.assertIsNone(self.client.query_parameters)
+        self.assertEqual('',self.client.query_parameters)
 
     @unittest.skipIf(sys.version_info > (2, 7), "signature assertion not supported for this version")
     def test_sign_key(self):
